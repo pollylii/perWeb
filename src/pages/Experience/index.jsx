e47bf0a850a548b57bc99300520f43532705bea1
pollylilh <794211268@qq.com>
2021-07-01 14:10:59 +0800

    Experience

--- src/pages/Experience/index.jsx
+++ src/pages/Experience/index.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import { Timeline } from 'antd';
+import { ClockCircleOutlined } from '@ant-design/icons';
+import './index.scss'
 
-export default class Experience extends Component {
+//定义UI组件
+class Experience extends Component {
     render() {
         return (
-            <div>
-                Experience
+            <div className="experience">
+                <Timeline mode="alternate">
+                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                    <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
+                    <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                        beatae vitae dicta sunt explicabo.
+                    </Timeline.Item>
+                    <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
+                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                    <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
+                        Technical testing 2015-09-01
+                    </Timeline.Item>
+                </Timeline>,
             </div>
         )
     }
 }
+export default Experience
+
