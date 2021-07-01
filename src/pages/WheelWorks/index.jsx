fed67ab396f1275cd63091965c4df8f766775663
pollylilh <794211268@qq.com>
2021-07-01 14:01:27 +0800

    wheelWorks

--- src/pages/WheelWorks/index.jsx
+++ src/pages/WheelWorks/index.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import { Card, Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import './index.scss'
 
 export default class WheelWorks extends Component {
     render() {
+        const { Meta } = Card;
         return (
-            <div>
-                WheelWorks
+            <div className="wheelWorks">
+                <div>
+                    {
+                        [1, 2, 3].map(item => {
+                            return (
+                                <Card
+                                    key={item}
+                                    style={{ width: 800 }}
+                                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                                    actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />,]}
+                                >
+                                    <Meta
+                                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                        title="Card title"
+                                        description="This is the description"
+                                    />
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
+                
+
             </div>
         )
     }
