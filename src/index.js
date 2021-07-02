7ef1c0aae5e76ea5ab42e68a50b6881d041e3c27
pollylilh <794211268@qq.com>
2021-07-02 10:58:17 +0800

    完善代码

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+// 组件
+import Layout from './layout'
+// js
 import store from './redux/store'
-import {Provider} from 'react-redux'
+//  css
+import "antd/dist/antd.css";//引入样式
+import "./index.scss"
 
 ReactDOM.render(
 	<BrowserRouter>
 		<React.StrictMode>
 			{/* 此处需要用Provider包裹App，目的是让App所有的后代容器组件都能接收到store */}
 			<Provider store={store}>
-				<App/>
+				<Layout />
 			</Provider>
-    	</React.StrictMode>
-  	</BrowserRouter>,
+		</React.StrictMode>
+	</BrowserRouter>,
 	document.getElementById('root')
 )
